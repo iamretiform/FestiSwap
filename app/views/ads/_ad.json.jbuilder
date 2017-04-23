b4f473e85ec071053c7fc920c6afb75462e60d3e
Scott Plunkett <iamretiform@github.com>
2017-04-22 21:43:27 -0500

    created resources, routes, root landing page, view files for Events &Ads

---
+++ app/views/ads/_ad.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! ad, :id, :created_at, :updated_at
+json.url ad_url(ad, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :events do
+    resources :ads
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
