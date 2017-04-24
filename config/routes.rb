60e6d3f81b2a3f6642ba3a63219eedb80e5f42e7
Scott Plunkett <iamretiform@github.com>
2017-04-24 12:52:35 -0500

    updated views, next view issue: list ads on events show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :events do
     resources :ads
   end
-  root 'home#index'
-  resources :events do
-    end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
