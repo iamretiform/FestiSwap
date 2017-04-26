7dabaf31345a05837c14fb9ced1c07e3ea5a0320
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-26 09:41:57 -0500

    working

--- app/controllers/temp_ads_controller.rb
+++ app/controllers/temp_ads_controller.rb
@@ -1,55 +1,55 @@
-class TempAdsController < ApplicationController
-  before_action :set_user
-  before_action :set_ads
-  before_action :set_ad, only: %i[show edit update destroy]
+# class TempAdsController < ApplicationController
+#   before_action :set_user
+#   before_action :set_ads
+#   before_action :set_ad, only: %i[show edit update destroy]
 
-  def show; end
+#   def show; end
 
-  def new
-    @ad = @event.ads.build
-  end
+#   def new
+#     @ad = @event.ads.build
+#   end
 
-  def edit; end
+#   def edit; end
 
-  def create
-    @ad = @event.ads.new(ad_params)
+#   def create
+#     @ad = @event.ads.new(ad_params)
 
-    if @ad.save
-      redirect_to([@ad.event, @ad], notice: 'Ad was successfully created.')
-    else
-      render action: 'new'
-    end
-  end
+#     if @ad.save
+#       redirect_to([@ad.event, @ad], notice: 'Ad was successfully created.')
+#     else
+#       render action: 'new'
+#     end
+#   end
 
-  def update
-    if @ad.update_attributes(ad_params)
-      redirect_to([@ad.event, @ad], notice: 'Ad was successfully updated.')
-    else
-      render action: 'edit'
-    end
-  end
+#   def update
+#     if @ad.update_attributes(ad_params)
+#       redirect_to([@ad.event, @ad], notice: 'Ad was successfully updated.')
+#     else
+#       render action: 'edit'
+#     end
+#   end
 
-  def destroy
-    @ad.destroy
+#   def destroy
+#     @ad.destroy
 
-    redirect_to event_path(@event)
-  end
+#     redirect_to event_path(@event)
+#   end
 
-  private
+#   private
 
-  def set_ads
-    @event = Event.find(params[:event_id])
-  end
+#   def set_ads
+#     @event = Event.find(params[:event_id])
+#   end
 
-  def set_user
-    @user = current_user.id
-  end
+#   def set_user
+#     @user = current_user.id
+#   end
 
-  def set_ad
-    @ad = @event.ads.find(params[:id])
-  end
+#   def set_ad
+#     @ad = @event.ads.find(params[:id])
+#   end
 
-  def ad_params
-    params.require(:ad).permit(:title, :description).merge(event_id: @event, user_id: @user)
-  end
-end
+#   def ad_params
+#     params.require(:ad).permit(:title, :description).merge(event_id: @event, user_id: @user)
+#   end
+# end
