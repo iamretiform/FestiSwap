ad6f1b1e1a003fe9bc904727cffe75cdf61ac856
Scott Plunkett <iamretiform@github.com>
2017-04-26 13:36:46 -0500

    added creating user feature validations for editing and deleting events/ads

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
-  before_action :set_user
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_user, only: [:new, :create, :edit, :update, :destroy]
   before_action :find_event, only: %i[show edit update destroy]
 
   def index
