b03fff84b534afad3d07a2393ca6294b3191872a
Scott Plunkett <iamretiform@github.com>
2017-04-28 16:28:06 -0500

    added async loaded map w/ static marker map with user current location at center began pseudocoding for event circles to visualize geovalidation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -25,6 +25,39 @@ $(document).ready(function() {
     function locationSuccess(position) {
       latr.innerHTML = position.coords.latitude;
       longr.innerHTML = position.coords.longitude;
+      // create a loop that grabs all of the events latitude, longitude, and radius and feeds them into var eventMap JSON
+      // var eventMap = {
+      //   eventName: {
+      //     center: {lat: event.latitude, lng: event.longitude},
+      //     radius: event.radius
+      //   }
+      // };
+
+      $(function initMap() {
+        var uluru = { lat: position.coords.latitude, lng: position.coords.longitude };
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14,
+          center: uluru,
+          mapTypeId: 'hybrid'
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+        // for (var event in eventMap) {
+        //   // Add the circle for this city to the map.
+        //   var eventCircle = new google.maps.Circle({
+        //     strokeColor: '#FF0000',
+        //     strokeOpacity: 0.8,
+        //     strokeWeight: 2,
+        //     fillColor: '#FF0000',
+        //     fillOpacity: 0.35,
+        //     map: map,
+        //     center: {lat: event.latitude, lng: event.longitude},
+        //     radius: event.radius
+        //   });
+        // }
+      });
     }
 
     function locationFail() {
