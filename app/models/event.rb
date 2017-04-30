4a8fa97bcc4f2d2d37f19e261da03f9e1edad83c
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-04-30 17:17:22 -0500

    updated paperclip defaults

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,8 +5,8 @@ class Event < ApplicationRecord
   has_many :ads
   validates :title, :description, :user_id, :termination_date, presence: true
   has_attached_file :file, styles: { medium: '300x300>', thumb: '100x100>' },
-                           default_url: '/images/:style/missing_2.png',
-                           path: 'system/events/images/:id/:style/:basename.png',
-                           url: 'system/events/images/:id/:style/:basename.png'
+                           # default_url: '/images/:style/missing_2.png',
+                           # path: 'system/events/images/:id/:style/:basename.png',
+                           # url: 'system/events/images/:id/:style/:basename.png'
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
 end
