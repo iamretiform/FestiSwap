b5d7b0dd12d4f189352076a15fd159a5c2f780cd
Scott Plunkett <iamretiform@github.com>
2017-05-02 00:39:20 -0500

    map should work with markers now

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,50 +17,51 @@
 //= require jquery_ujs
 //= require turbolinks
 // require_tree .
-function initMap() {}
-$(document).ready(function() {
-  navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
 
-  function locationSuccess(position) {
-    latr.innerHTML = position.coords.latitude;
-    longr.innerHTML = position.coords.longitude;
-    $(document).ready(function initMap() {
-      var uluru = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
-        center: uluru,
-        mapTypeId: 'hybrid'
-      });
-      var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-      });
-      for (var i = 0; i <= 5; i++) {
-        console.log(Number(document.getElementById("event" + i + "_latitude").innerHTML));
-        console.log(Number(document.getElementById("event" + i + "_longitude").innerHTML));
-        console.log(Number(document.getElementById("event" + i + "_radius").innerHTML));
-        // Add the circle for this city to the map.
-        var eventCircle = new google.maps.Circle({
-          strokeColor: '#F26060 ',
-          strokeOpacity: 0.7,
-          strokeWeight: 1.5,
-          fillColor: '#F337B4 ',
-          fillOpacity: 0.15,
-          map: map,
-          center: {
-            lat: Number(document.getElementById("event" + i + "_latitude").innerHTML),
-            lng: Number(document.getElementById("event" + i + "_longitude").innerHTML)
-          },
-          radius: Number(document.getElementById("event" + i + "_radius").innerHTML) * 200
-        });
-      }
+
+function initMap() {
+}
+
+function locationSuccess(position) {
+  latr.innerHTML = position.coords.latitude;
+  longr.innerHTML = position.coords.longitude;
+  initMap();
+  var uluru = {
+    lat: position.coords.latitude,
+    lng: position.coords.longitude
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: uluru,
+    mapTypeId: 'hybrid'
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+  for (var i = 0; i <= 5; i++) {
+    console.log(Number(document.getElementById("event" + i + "_latitude").innerHTML));
+    console.log(Number(document.getElementById("event" + i + "_longitude").innerHTML));
+    console.log(Number(document.getElementById("event" + i + "_radius").innerHTML));
+    // Add the circle for this city to the map.
+    var eventCircle = new google.maps.Circle({
+      strokeColor: '#F26060 ',
+      strokeOpacity: 0.7,
+      strokeWeight: 1.5,
+      fillColor: '#F337B4 ',
+      fillOpacity: 0.15,
+      map: map,
+      center: {
+        lat: Number(document.getElementById("event" + i + "_latitude").innerHTML),
+        lng: Number(document.getElementById("event" + i + "_longitude").innerHTML)
+      },
+      radius: Number(document.getElementById("event" + i + "_radius").innerHTML) * 200
     });
   }
+}
 
-  function locationFail() {
-    alert("Oops, could not find you. Check your browser settings and enable location services.  Or you won't get to swap... and swapping rules.");
-  }
-});
+function locationFail() {
+  alert("Oops, could not find you. Check your browser settings and enable location services.  Or you won't get to swap... and swapping rules.");
+}
+$(document).ready(() =>
+  navigator.geolocation.getCurrentPosition(locationSuccess, locationFail));
