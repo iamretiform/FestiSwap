692e8abac59bb682c3c3ac7f38a2656af901652f
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-05-02 12:31:04 -0500

    added redirect to https

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_to_https
   before_action :authenticate_user!, only: %i[delete_user_photo]
   before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :null_session
@@ -13,5 +14,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password about locale avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name about locale avatar])
+  end
+
+  def redirect_to_https
+      redirect_to :protocol => "https://" unless (request.ssl? || request.local?)
   end
 end
