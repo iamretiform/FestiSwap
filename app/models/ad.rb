edbb9ae76f305c35008a1b414693b5cf95084b85
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-05-02 11:57:31 -0500

    fixed default images

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,6 @@ class Ad < ApplicationRecord
   belongs_to :event
   belongs_to :user
   validates :title, :description, :user_id, :termination_date, presence: true
-  has_attached_file :file, styles: { medium: '300x300>', thumb: '100x100>' }
+  has_attached_file :file, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: "/images/:style/missing_2.png"
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
 end
