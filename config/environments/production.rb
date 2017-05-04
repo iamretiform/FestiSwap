b382a65d039035aa0d3687b4c692f247ab479ad5
Daniel <dkweonlee@gmail.com>
2017-05-04 12:30:05 -0500

    updated email function

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  Rails.application.routes.default_url_options[:host] = 'festiswap.herokuapp.com'
+
   config.action_mailer.default_url_options = { host: 'festiswap.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
