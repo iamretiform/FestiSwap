88ca0bfa16272d57e3434ff22aa3c28aa521d9a3
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-05-04 12:46:23 -0500

    merge

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,3 @@
 Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-# Paperclip::Attachment.default_options[:default_url] = "/images/:style/default_image.png"
 Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 Paperclip::Attachment.default_options[:s3_host_name] = 's3.us-east-2.amazonaws.com'
