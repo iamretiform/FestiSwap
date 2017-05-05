fd3d7cfb290c33c076b77e574fbea34b30db06e3
CONLIN MCMANUS <conlinmcmanus@gmail.com>
2017-05-05 13:30:46 -0500

    added footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:passwords => "passwords"}, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations', :passwords => "passwords" }
   # get 'hello_world', to: 'hello_world#index'
   root 'home#index'
   resources :events do
